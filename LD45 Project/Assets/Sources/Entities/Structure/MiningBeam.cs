ba421c22e99ace0f87b8486547605d17ce556e48
MarcJacob <marcjacobpro@gmail.com>
2019-10-07 12:35:44 +0200

    Ajout FX minage. Postprocessing (bloom).

--- LD45 Project/Assets/Sources/Entities/Structure/MiningBeam.cs
+++ LD45 Project/Assets/Sources/Entities/Structure/MiningBeam.cs
@@ -12,6 +12,10 @@ public class MiningBeam : MonoBehaviour
     [SerializeField]
     private Cargo cargo;
 
+    public event Action<Vector2, GameObject> OnActivation = delegate { };
+    public event Action OnDeactivation = delegate { };
+    public event Action<Vector2, GameObject> OnRunning = delegate { }; // Called each frame while the beam is active.
+
     private ShipPiloting shipControllerInput;
 
     float[] mineBuffer;
@@ -43,31 +47,59 @@ public class MiningBeam : MonoBehaviour
 
                 if (mineable.ResourceType == mineableResource && (transform.position - target.transform.position).sqrMagnitude < range * range)
                 {
-                    Debug.DrawLine(transform.position, shipControllerInput.CurrentPilot.targetedPosition + (Vector2)transform.position);
-                    mineBuffer[(uint)mineableResource] += miningStrength * Time.deltaTime;
-                    if (mineBuffer[(uint)mineableResource] >= 1f)
-                    {
-                        uint minedInBuffer = (uint)Mathf.FloorToInt(mineBuffer[(uint)mineableResource]);
-                        mineBuffer[(uint)mineableResource] -= minedInBuffer;
-
-                        uint mined = mineable.Mine((uint)mineableResource, minedInBuffer);
-                        if (!cargo.AddResource((uint)mineableResource, mined))
-                        {
-                            Debug.LogWarning("Warning - Not enough cargo to fit mined resources ! Yield has been lost.");
-                        }
-                    }
+                    ActivateMiningOn(mineable);
                 }
                 else
                 {
                     Debug.DrawLine(transform.position, shipControllerInput.CurrentPilot.targetedPosition + (Vector2)transform.position, Color.red);
-
+                    DeactivateMining();
                 }
             }
             else
             {
-                for (int i = 0; i < mineBuffer.Length; i++)
-                    mineBuffer[i] = 0f;
+                DeactivateMining();
             }
+        }
+        else if (activatedLastFrame)
+        {
+            DeactivateMining();
+        }
+    }
+    private bool activatedLastFrame = false;
+    private void DeactivateMining()
+    {
+        for (int i = 0; i < mineBuffer.Length; i++)
+            mineBuffer[i] = 0f;
+        if (activatedLastFrame)
+        {
+            activatedLastFrame = false;
+            OnDeactivation();
+        }
+    }
+
+    private void ActivateMiningOn(Mineable mineable)
+    {
+        Debug.DrawLine(transform.position, shipControllerInput.CurrentPilot.targetedPosition + (Vector2)transform.position);
+        mineBuffer[(uint)mineableResource] += miningStrength * Time.deltaTime;
+        if (mineBuffer[(uint)mineableResource] >= 1f)
+        {
+            uint minedInBuffer = (uint)Mathf.FloorToInt(mineBuffer[(uint)mineableResource]);
+            mineBuffer[(uint)mineableResource] -= minedInBuffer;
+
+            uint mined = mineable.Mine((uint)mineableResource, minedInBuffer);
+            if (!cargo.AddResource((uint)mineableResource, mined))
+            {
+                Debug.LogWarning("Warning - Not enough cargo to fit mined resources ! Yield has been lost.");
+            }
+        }
+        if (!activatedLastFrame)
+        {
+            activatedLastFrame = true;
+            OnActivation(shipControllerInput.CurrentPilot.targetedPosition + (Vector2)transform.position, mineable.gameObject);
+        }
+        else
+        {
+            OnRunning(shipControllerInput.CurrentPilot.targetedPosition + (Vector2)transform.position, mineable.gameObject);
         }
     }
 }
---
+++ LD45 Project/Assets/Sources/Graphics/BeamFX.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MiningBeam))]
+public class BeamFX : MonoBehaviour
+{
+    private MiningBeam miningBeam;
+
+    [SerializeField]
+    private ParticleSystem targetParticleFX;
+    [SerializeField]
+    private LineRenderer beamFX;
+
+    ParticleSystem.EmissionModule fxEmission;
+    Vector2 beamCurrentPosition;
+
+    private void Awake()
+    {
+        miningBeam = GetComponent<MiningBeam>();
+        miningBeam.OnActivation += MiningBeam_OnActivation;
+        miningBeam.OnDeactivation += MiningBeam_OnDeactivation;
+        miningBeam.OnRunning += MiningBeam_OnRunning;
+
+        targetParticleFX = Instantiate(targetParticleFX, transform);
+        beamFX = Instantiate(beamFX, transform);
+        fxEmission = targetParticleFX.emission;
+        fxEmission.enabled = false;
+
+        beamFX.sortingLayerName = "Ships";
+        beamFX.sortingOrder = -1;
+    }
+
+    bool beamActivated = false;
+    private void Update()
+    {
+        if (beamActivated == false)
+        {
+            beamCurrentPosition = transform.position;
+        }
+        beamFX.SetPosition(0, transform.position);
+        beamFX.SetPosition(1, Vector2.Lerp(beamFX.GetPosition(1), beamCurrentPosition, Time.deltaTime * 10f));
+
+
+    }
+
+    // Update position
+    private void MiningBeam_OnRunning(Vector2 pos, GameObject arg2)
+    {
+        targetParticleFX.transform.position = pos;
+        beamCurrentPosition = pos;
+    }
+
+    // Stop emitting
+    private void MiningBeam_OnDeactivation()
+    {
+        fxEmission.enabled = false;
+        beamActivated = false;
+    }
+
+    // Start emitting
+    private void MiningBeam_OnActivation(Vector2 pos, GameObject arg2)
+    {
+        fxEmission.enabled = true;
+        targetParticleFX.transform.position = pos;
+        beamCurrentPosition = pos;
+        beamActivated = true;
+    }
+}
